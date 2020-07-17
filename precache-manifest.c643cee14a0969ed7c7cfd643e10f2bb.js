2a8e96fdecfb666e92efc81d99f0b274688e0b10
Asia Meus <joanna@meus.pl>
2020-07-17 13:13:48 +0200

    Updates

---
+++ precache-manifest.c643cee14a0969ed7c7cfd643e10f2bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd8e77ab7e32dee856f48c51859bbae",
+    "url": "/to-do-list-react/index.html"
+  },
+  {
+    "revision": "c8a772756d6d903d0a65",
+    "url": "/to-do-list-react/static/css/main.c544037d.chunk.css"
+  },
+  {
+    "revision": "56ab7f46fb79e61aaa1e",
+    "url": "/to-do-list-react/static/js/2.ccf67aed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list-react/static/js/2.ccf67aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a772756d6d903d0a65",
+    "url": "/to-do-list-react/static/js/main.39e0fc18.chunk.js"
+  },
+  {
+    "revision": "4f831eb39a375a9667d0",
+    "url": "/to-do-list-react/static/js/runtime-main.55ffe52c.js"
+  }
+]);
